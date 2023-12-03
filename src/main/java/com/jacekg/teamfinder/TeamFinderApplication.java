a7d4f18d0fba29ca9ba94924752e2fd28078aee2
JacekGos <jacekg93@gmail.com>
2023-12-03 12:09:41 +0100

    Added properties file for secure data, updated gitignore file

--- src/main/java/com/jacekg/teamfinder/TeamFinderApplication.java
+++ src/main/java/com/jacekg/teamfinder/TeamFinderApplication.java
@@ -2,8 +2,11 @@ package com.jacekg.teamfinder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:application.properties")
+@PropertySource("classpath:secrets.properties")
 public class TeamFinderApplication {
 
 	public static void main(String[] args) {
