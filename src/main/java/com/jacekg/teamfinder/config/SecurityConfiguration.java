2dec6384b2f821e56e1d47f01660bb7d795637ee
JacekGos <jacekg93@gmail.com>
2023-12-14 20:13:42 +0100

    Added logs for SecurityConfiguration

--- src/main/java/com/jacekg/teamfinder/config/SecurityConfiguration.java
+++ src/main/java/com/jacekg/teamfinder/config/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.jacekg.teamfinder.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +16,7 @@ import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
+@Slf4j
 public class SecurityConfiguration {
 
     @Value("${aws.ui.url}") String AWS_UI_URL;
@@ -26,6 +28,7 @@ public class SecurityConfiguration {
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/v1/*").permitAll())
+
                 .build();
     }
 
