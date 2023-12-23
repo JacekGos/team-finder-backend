3556bfc64fe51bbf7eabcd79536b650970ba9690
JacekGos <jacekg93@gmail.com>
2023-12-23 11:51:00 +0100

    Updated properties for continoues deployment

--- src/main/java/com/jacekg/teamfinder/TeamFinderApplication.java
+++ src/main/java/com/jacekg/teamfinder/TeamFinderApplication.java
@@ -7,8 +7,6 @@ import org.springframework.context.annotation.PropertySource;
 @SpringBootApplication
 @PropertySource("classpath:application.properties")
 @PropertySource("classpath:application-docker.properties")
-@PropertySource("classpath:secrets.properties")
-@PropertySource("classpath:secrets-docker.properties")
 public class TeamFinderApplication {
 
 	public static void main(String[] args) {
